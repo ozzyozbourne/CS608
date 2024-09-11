c2ecae13daf02c408b64cdb8d2a3f9d4f437f015
ozzyozbourne <khanosaid726@gmail.com>
2024-09-11 15:03:42 -0400

    kadane sliding seems to be working now

--- java/src/Main.java
+++ java/src/Main.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 
 final public class Main {
 
-    private record SumAndIndex(int l, int h, int sum){}
+    private record SumAndIndex(int l, int h, int sum) {
+    }
+
     private int[] arr;
-    private Main() {}
+
+    private Main() {
+    }
 
     private SumAndIndex maxSubArraySum(int l, int h) {
-        //base case
-        if (l == h) return new SumAndIndex(l, h, arr[l]);
+        // base case
+        if (l == h)
+            return new SumAndIndex(l, h, arr[l]);
 
         // To avoid int overflow
         final int mid = l + (h - l) / 2;
@@ -18,7 +23,7 @@ final public class Main {
         final var leftMaxSum = maxSubArraySum(l, mid);
         final var rightMaxSum = maxSubArraySum(mid + 1, h);
         // find the max sum of the crossing
-        final var crossingMaxSum =maxCrossingSum(l, mid, h);
+        final var crossingMaxSum = maxCrossingSum(l, mid, h);
 
         // get the max of the left and right side of the mid
         final var maxOfLeftAndRight = leftMaxSum.sum > rightMaxSum.sum ? leftMaxSum : rightMaxSum;
@@ -29,29 +34,30 @@ final public class Main {
 
     private SumAndIndex maxCrossingSum(final int l, final int m, final int h) {
 
-        //calculate the sum from the mid to the left of the array
-        // also get the index from where the starting from l to mid here we find the max sum
+        // calculate the sum from the mid to the left of the array
+        // also get the index from where the starting from l to mid here we find the max
+        // sum
         int sum = 0, lSum = Integer.MIN_VALUE, maxLeft = m;
         for (int i = m; i >= l; i--) {
             sum += this.arr[i];
-            if (sum > lSum){
+            if (sum > lSum) {
                 lSum = sum;
                 maxLeft = i;
             }
         }
 
-        //calculate the sum from mid to the right of the array
+        // calculate the sum from mid to the right of the array
         sum = 0;
         int rSum = Integer.MIN_VALUE, maxRight = m + 1;
         for (int i = m + 1; i <= h; i++) {
             sum += this.arr[i];
-            if (sum > rSum){
+            if (sum > rSum) {
                 rSum = sum;
                 maxRight = i;
             }
         }
         // the index and the sum the max crossing sum
-        return new SumAndIndex(maxLeft, maxRight, lSum+ rSum);
+        return new SumAndIndex(maxLeft, maxRight, lSum + rSum);
 
     }
 
@@ -59,17 +65,25 @@ final public class Main {
         final Main mainTest1 = new Main();
 
         // this also works for negative number
-        mainTest1.arr = new int[]{-1, -2, -3, -4, -5};
+        mainTest1.arr = new int[] { -10, -15, -3, -20, -11 };
         System.out.println(mainTest1.maxSubArraySum(0, mainTest1.arr.length - 1));
+        System.out.println(mainTest1.kadaneSliding());
 
-//        getSizeOfTheArray(mainTest1);
-//        fillArrayWithRandomNumbers(mainTest1, 200);
-//        System.out.println(mainTest1.maxSubArraySum(0, mainTest1.arr.length - 1));
+        System.out.println("--------------------------------------------------");
+        mainTest1.arr = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        System.out.println(mainTest1.maxSubArraySum(0, mainTest1.arr.length - 1));
+        System.out.println(mainTest1.kadaneSliding());
+        System.out.println("--------------------------------------------------");
+
+        // getSizeOfTheArray(mainTest1);
+        // fillArrayWithRandomNumbers(mainTest1, 200);
+        // System.out.println(mainTest1.maxSubArraySum(0, mainTest1.arr.length - 1));
 
     }
 
-    // util functions start from here ------------------------------------------------
-    private static void getSizeOfTheArray(final Main main){
+    // util functions start from here
+    // ------------------------------------------------
+    private static void getSizeOfTheArray(final Main main) {
         final Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array");
 
@@ -82,33 +96,39 @@ final public class Main {
 
     private static void fillArrayWithRandomNumbers(final Main main, final int rangeOfNumber) {
         final Random rand = new Random();
-        for (int i = 0; i < main.arr.length; i++) main.arr[i] = rand.nextInt(rangeOfNumber) - 100;
+        for (int i = 0; i < main.arr.length; i++)
+            main.arr[i] = rand.nextInt(rangeOfNumber) - 100;
         System.out.println("The input Array is -> ");
-        for(final var num : main.arr) System.out.print(num + " ");
+        for (final var num : main.arr)
+            System.out.print(num + " ");
         System.out.println();
     }
 
-
-    private int kadane(){
+    private int kadane() {
         var maxSum = this.arr[0];
         var currSum = Integer.MIN_VALUE;
-        for(final int num : this.arr) {
+        for (final int num : this.arr) {
             currSum += Math.max(0, currSum) + num;
             maxSum = Math.max(maxSum, currSum);
-        }return maxSum;
+        }
+        return maxSum;
     }
 
-    private SumAndIndex kadaneSliding(){
+    private SumAndIndex kadaneSliding() {
         int maxl = 0, maxr = 0, l = 0, currSum = 0, maxSum = this.arr[0];
-        for(int r = 0; r < this.arr.length; r++) {
-            if (currSum > maxSum){
+        for (int r = 0; r < this.arr.length; r++) {
+            if (currSum < 0) {
+                currSum = 0;
+                l = r;
+            }
+            currSum += this.arr[r];
+            if (currSum > maxSum) {
                 maxSum = currSum;
                 maxl = l;
                 maxr = r;
             }
-            currSum += Math.max(0, currSum) + this.arr[r];
-
-        }return new SumAndIndex(maxl, maxr, maxSum);
+        }
+        return new SumAndIndex(maxl, maxr, maxSum);
     }
 
 }
