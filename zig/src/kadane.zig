c56dae04c1b21c5e6567c472a70f475f5c1f5bd8
ozzyozbourne <khanosaid726@gmail.com>
2024-09-11 17:19:44 -0400

    kadane implemented in zig

--- zig/src/kadane.zig
+++ zig/src/kadane.zig
@@ -4,6 +4,7 @@
 
 const std = @import("std");
 const testing = std.testing;
+const Res = struct { l: usize, h: usize, sum: i32 };
 
 fn max(a: i32, b: i32) i32 {
     return if (a > b) a else b;
@@ -35,6 +36,73 @@ pub fn maxSubArrayLocation(arr: []const i32) struct { l: i32, r: i32 } {
     }
 
     return .{ .l = maxl, .r = maxr };
+}
+
+pub fn maxSubArray(l: usize, h: usize, arr: []const i32) Res {
+    // bases case
+    if (l == h) return Res{ .l = l, .h = h, .sum = arr[l] };
+    // calculate the mid for current l and h
+    const mid: usize = l + @divTrunc(h - l, 2);
+    //calcute the current leftmax and right max the max from mid to low and mid to high
+    const leftMax = maxSubArray(l, mid, arr);
+    const rightMax = maxSubArray(mid + 1, h, arr);
+    const maxcrossing = maxCrossing(l, mid, h, arr);
+
+    const maxofleftandright = if (leftMax.sum > rightMax.sum) leftMax else rightMax;
+    return if (maxofleftandright.sum > maxcrossing.sum) maxofleftandright else maxcrossing;
+}
+
+fn maxCrossing(l: usize, mid: usize, h: usize, arr: []const i32) Res {
+    
+    var sum: i32, var lsum: i32, var maxleft: usize = .{ 0, std.math.minInt(i32), mid };
+
+    var iter: i32 = @intCast(mid);
+    const stop: i32 = @intCast(l);
+    
+    while(iter >= stop) {
+        sum += arr[@intCast(iter)];
+        if (sum > lsum) {
+            lsum = sum;
+            maxleft = @intCast(iter);
+        }
+        iter -= 1;
+    }
+
+    sum, var rsum: i32, var maxright: usize = .{ 0, std.math.minInt(i32), mid + 1 };
+
+    for ((mid + 1)..(h + 1)) |i| {
+        sum += arr[i];
+        if (sum > rsum) {
+            rsum = sum;
+            maxright = i;
+        }
+    }
+
+    return Res{ .l = maxleft, .h = maxright, .sum = lsum + rsum };
+}
+
+test "Divide and conquer Algorighm for max subarray sum" {
+    const TestCase = struct {
+        input: []const i32,
+        expected: Res,
+    };
+    const testcases = [_]TestCase{
+        TestCase{ .input = &[_]i32{ -2, 1, -3, 4, -1, 2, 1, -5, 4 }, .expected = Res{ .l = 3, .h = 6, .sum = 6 } },
+        TestCase{ .input = &[_]i32{1},                               .expected = Res{ .l = 0, .h = 0, .sum = 1 } },
+        TestCase{ .input = &[_]i32{ 5, 4, -1, 7, 8 },                .expected = Res{ .l = 0, .h = 4, .sum = 23 } },
+    };
+
+    for (testcases) |testcase| {
+        const res = maxSubArray(0, @intCast(testcase.input.len - 1), testcase.input);
+//        std.debug.print("{} {} {}\n", .{res.l, res.h, res.sum});
+        try testing.expect(
+            res.l == testcase.expected.l and 
+            res.h == testcase.expected.h and 
+            res.sum == testcase.expected.sum
+        );
+    }
+
+    std.debug.print("All test cases passed!\n", .{});
 }
 
 test "Kadane Algorighm for max subarray sum" {
