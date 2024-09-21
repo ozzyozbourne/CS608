31200c0887abf261358072f3116e7e0bac962233
ozzyozbourne <khanosaid726@gmail.com>
2024-09-21 15:01:34 -0400

    added extra credit class

---
+++ java/src/BSTExtraCredit.java
@@ -0,0 +1,11 @@
+public final class BSTExtraCredit {
+
+    // this class contains only static fuctions no need to create an object
+    private BSTExtraCredit() {
+
+    }
+
+    public static void main(final String... args) {
+
+    }
+}
