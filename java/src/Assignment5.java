157f4944e6177a4f6f4feb469547571ae644aa99
ozzyozbourne <khanosaid726@gmail.com>
2024-10-27 18:04:43 -0400

    starting assignment 5

---
+++ java/src/Assignment5.java
@@ -0,0 +1,13 @@
+public final class Assignment5 {
+
+    private static record Points(double x, double y, double distance) {
+    }
+
+    private Assignment5() {
+
+    }
+
+    public static void main(final String... args) {
+
+    }
+}
